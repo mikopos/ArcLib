cfbc8ec0ec1606ed0fea788a06cdf677ec11d703
Marios <mikopos@gmail.com>
2019-02-18 23:23:01 +0200

    added ts folder but seems somethings goes really wrong

---
+++ public/ts/map.ts
@@ -0,0 +1,25 @@
+
+let raster = new ol.layer.Tile({
+    source: new ol.source.OSM()
+});
+
+let sourceLayer = new ol.source.Vector({
+    wrapX:false
+});
+
+let vectorLayer = new ol.layer.Vector({
+    source: sourceLayer
+});
+
+let map = new ol.Map({
+    layers: [raster, vectorLayer],
+    target: 'map',
+    view: new ol.View({
+        center: [1163747.714729, 6683705.090302],
+        zoom: 4
+    })
+});
+
+map.on('click', function(evt){
+    console.info(evt.coordinate);
+});
